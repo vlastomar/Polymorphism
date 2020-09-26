642af56845df540fefb70e6a53b6eb4499ca9887
VSM <vlad24@abv.bg>
2020-09-26 11:39:39 +0300

    math operation

---
+++ MathOperationPack/MathOperation.java
@@ -0,0 +1,38 @@
+package MathOperationPack;
+
+public class MathOperation {
+    private int first, second, third, fourth;
+
+    /*public MathOperation(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public MathOperation(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public MathOperation(int first, int second, int third, int fourth) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
+    }*/
+
+
+
+
+    public int add(int i, int i1) {
+        return i + i1;
+    }
+
+    public int add(int first, int second, int third) {
+        return first + second + third;
+    }
+
+    public int add(int first, int second, int third, int fourth) {
+        return first + second + third + fourth;
+    }
+}
